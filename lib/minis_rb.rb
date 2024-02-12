ee5a06f1101d80655a7113b2052dbc2a309c9ffa
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2024-02-12 12:40:29 +0900

    Clean up README

---
+++ lib/minis_rb.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "minis_rb/version"
+
+module MinisRb
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/minis_rb_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe MinisRb do
+  it "has a version number" do
+    expect(MinisRb::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
